d881f05fb9e2d1c0dd802dfa985d90f10119aa9b
Akshay Tilekar <akshaytilekar7@gmail.com>
2020-12-25 12:28:14 +0530

    C# topics and MS

---
+++ DesignPattern/Microservices/MS-5.cs
@@ -0,0 +1,32 @@
+﻿/*
+
+Types of communication
+    -   synchronous
+        -   Request and response
+        -   not related to async await
+            -   we cant proceed further until we get response from call
+        -   REST
+        -   gRPC (higher speed)
+            -   can provide streams of data
+            -   .proto file
+                -   contract-
+                -   client n server code is auto generated
+                -   define messages and services
+        -   signalR
+        -   TCP (fast)
+
+        -   Disadvantages
+            -   tight coupling
+            -   one to many (publish event is harder)
+            -   changes are hard
+            -   error track is difficult in case if MS1 calls MS2 calls MS3 
+            -   long chain of synchronous call (MS1 calls MS2 calls MS3) takes long time
+
+
+    -   asynchronous
+        -   
+
+
+
+
+*/ 
