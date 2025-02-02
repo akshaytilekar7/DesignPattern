1b82d4e6b515fe7afea8a92d9ccd6629a67d1dbf
akshaytilekar7 <akshaytilekar7@gmail.com>
2025-02-02 14:18:41 +0530

    asd

--- DIP/Program.cs
+++ DIP/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             Console.WriteLine("Hello, World!");
         }
