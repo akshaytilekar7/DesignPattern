a5eea211b17e89e747932e42c93a1439b76747ed
akshaytilekar7 <akshaytilekar7@gmail.com>
2025-02-16 19:43:19 +0530

    code clean up : move project to respective folder

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("");
+        Console.WriteLine("ENDDDDDDD");
         Console.ReadLine();
     }
 }
